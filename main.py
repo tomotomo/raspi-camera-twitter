14f4b40769b5320b1f7174e027d271c1125f1889
tomotomo <sugita@xshell.io>
2018-06-18 14:33:46 +0900

    flask

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-from time import sleep
+from flask import Flask, render_template, Response
+from processor.simple_streamer import SimpleStreamer as VideoCamera
+# from processor.motion_detector import MotionDetector as VideoCamera
+# from processor.person_detector import PersonDetector as VideoCamera
 
-while True:
-  print "hello isaax"
-  sleep(5)
+import time
+import threading
+
+video_camera = VideoCamera(flip=False)
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(video_camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=False, threaded=True)
